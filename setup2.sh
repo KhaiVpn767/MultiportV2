cc219d91704b1872440537b608714dc4301ba16e
KhaiVpn767 <112326124+KhaiVpn767@users.noreply.github.com>
2024-01-09 01:34:29 +0800

    Update setup2.sh

--- setup2.sh
+++ setup2.sh
@@ -1,35 +1,6 @@
 #!/bin/bash
 #wget https://github.com/${GitUser}/
-GitUser="khaiVPN"
-# Color Validation
-Lred='\e[1;91m'
-Lgreen='\e[92m'
-Lyellow='\e[93m'
-green='\e[32m'
-RED='\033[0;31m'
-NC='\033[0m'
-BGBLUE='\e[1;44m'
-ORANGE='\033[0;33m'
-BLUE='\033[0;34m'
-PURPLE='\033[0;35m'
-CYAN='\033[0;36m'
-NC='\033[0;37m'
-# ===================
-echo ''
-clear
-echo ''
-echo "                                                              "
-echo -e "$Lyellow                ⚡ PREMIUM SPEED SCRIPT ⚡"$NC
-echo -e "$green.........................................................."$NC
-echo -e "$Lyellow                  Autoscript By khaiVPN"$NC
-echo -e "$Lyellow                    CONTACT TELEGRAM"$NC
-echo -e "$Lyellow                       @khaivpn"$NC
-echo -e "$green.........................................................."$NC
-echo ''
-echo -e "$Lyellow                       Wait 6 Seconds!"$NC
-echo -e "$green.........................................................."$NC
-sleep 6
-clear
+GitUser="KhaiVpn767"
 if [ "${EUID}" -ne 0 ]; then
 		echo "You need to run this script as root"
 		exit 1
@@ -42,44 +13,39 @@ red='\e[1;31m'
 green='\e[0;32m'
 NC='\e[0m'
 #IZIN SCRIPT
-MYIP=$(curl -sS ipv4.icanhazip.com)
-echo -e "\e[32mloading...\e[0m"
+MYIP=$(wget -qO- ipinfo.io/ip);
+echo "                                                              "
+echo -e "$Lyellow                ⚡ PREMIUM SPEED SCRIPT ⚡"$NC
+echo -e "$green.........................................................."$NC
+echo -e "$Lyellow                  Autoscript By khaiVPN"$NC
+echo -e "$Lyellow                    CONTACT TELEGRAM"$NC
+echo -e "$Lyellow                       @khaivpn"$NC
+echo -e "$green.........................................................."$NC
+echo ''
+echo -e "$Lyellow                       Wait 6 Seconds!"$NC
+echo -e "$green.........................................................."$NC
+sleep 6
+# Insert Password
+Password=khaivpnking
+# Execute
+read -p "Please Insert The License Key : " Passwordnya
+if [ $Password = $Passwordnya ]; then
 clear
-# Valid Script
-VALIDITY() {
-    today=$(date -d "0 days" +"%Y-%m-%d")
-    Exp1=$(curl https://raw.githubusercontent.com/${GitUser}/allow/main/ipvps.conf | grep $MYIP | awk '{print $4}')
-    if [[ $today < $Exp1 ]]; then
-        echo -e "\e[32mCongratulations! You are Allowed to use AUTOSCRIPT khaiVPN..\e[0m"
-        sleep 5
-    else
-        echo -e "\e[31mYOUR SCRIPT HAS EXPIRED!\e[0m"
-        echo -e "\e[31mPlease renew your ipvps first\e[0m"
-        exit 0
-    fi
-}
-# Valid Script
-VALIDITY() {
-    today=$(date -d "0 days" +"%Y-%m-%d")
-    Exp1=$(curl https://raw.githubusercontent.com/${GitUser}/allow/main/ipvps.conf | grep $MYIP | awk '{print $4}')
-    if [[ $today < $Exp1 ]]; then
-        echo -e "\e[32mCongratulations! You are Allowed to use AUTOSCRIPT khaiVPN..\e[0m"
-        sleep 5
-    else
-        echo -e "\e[31mYOUR SCRIPT HAS EXPIRED!\e[0m"
-        echo -e "\e[31mPlease renew your ipvps first\e[0m"
-        exit 0
-    fi
-}
-IZIN=$(curl https://raw.githubusercontent.com/${GitUser}/allow/main/ipvps.conf | awk '{print $5}' | grep $MYIP)
-if [ $MYIP = $IZIN ]; then
-    echo -e "\e[32mPermission Accepted...\e[0m"
-    VALIDITY
+echo -e ""
+echo -e "${green}Permission Accepted...${NC}"
+echo ""
+echo "Thanks For Using This Autoscript-Lite By khaivpn"
+echo ""
+sleep 3
 else
-    echo -e "\e[31mPermission Denied!\e[0m"
-    echo -e "\e[31mPlease buy script first\e[0m"
-    rm -f setup.sh
-    exit 0
+clear
+echo -e "${red}Permission Denied!${NC}";
+echo -e "${red}Please Insert The Correct License Key !${NC}"
+echo ""
+echo -e "Please Contact ${green}Admin${NC}"
+echo -e "Telegram : t.me/KhaiVpn767"
+rm -f setup2.sh
+exit 0
 fi
 clear
 echo -e "\e[32mloading...\e[0m"
