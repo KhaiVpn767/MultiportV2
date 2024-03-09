f07532c527653c23ddd752b2b2339f0de8a3029e
KhaiVpn767 <112326124+KhaiVpn767@users.noreply.github.com>
2024-03-09 15:37:01 +0800

    Update xraay2.sh

--- menu/xraay2.sh
+++ menu/xraay2.sh
@@ -1640,10 +1640,9 @@ echo -e "    \e[$number (18)\e[m \e[$below Check User Login Vless Xtls\e[m"
 echo -e ""
 echo -e "   \e[$number    >> Total :\e[m \e[$below ${total3} Client\e[m"
 echo -e "   \e[$line══════════════════════════════════════════\e[m"
-echo -e "   \e[$back_text  \e[$box x)   MENU                              \e[m"
-echo -e "   \e[$line══════════════════════════════════════════\e[m"
-echo -e "\e[$line"
-read -rp "        Please Input Number  [1-18 or x] :  "  num
+echo -e "    \e[$number (•0)\e[m \e[$below MENU\e[m"
+echo -e ""
+read -rp "        Please Input Number  [1-18 or 0] :  "  num
 echo -e ""
 if [[ "$num" = "1" ]]; then
 menu1
@@ -1681,7 +1680,7 @@ elif [[ "$num" = "17" ]]; then
 menu17
 elif [[ "$num" = "18" ]]; then
 menu18
-elif [[ "$num" = "x" ]]; then
+elif [[ "$num" = "0" ]]; then
 menu
 else
 clear
