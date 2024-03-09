e8b4442ed53c6cc11093e8a6b3f7c7225cb57655
KhaiVpn767 <112326124+KhaiVpn767@users.noreply.github.com>
2024-03-09 15:52:46 +0800

    Update panel-domain.sh

--- menu/panel-domain.sh
+++ menu/panel-domain.sh
@@ -52,11 +52,10 @@ echo -e "   \e[$number (•1)\e[m \e[$below Add ID Cloudflare\e[m"
 echo -e "   \e[$number (•2)\e[m \e[$below Add Subdomain\e[m"
 echo -e "   \e[$number (•3)\e[m \e[$below Pointing IP\e[m"
 echo -e ""
-echo -e "   \e[$line════════════════════════════════════════\e[m"
-echo -e "   \e[$back_text \e[$box x) SYSTEM MENU                        \e[m"
-echo -e "   \e[$line════════════════════════════════════════\e[m"
-echo -e "\e[$line"
-read -p "        Please Input Number  [1-3 or x] :  "  sys
+echo -e "   \e[$line══════════════════════════════════════════\e[m"
+echo -e "    \e[$number (•0)\e[m \e[$below MENU\e[m"
+echo -e ""
+read -p "        Please Input Number  [1-3 or 0] :  "  sys
 echo -e ""
 case $sys in
 1)
@@ -68,7 +67,7 @@ cfd
 3)
 cfp
 ;;
-x)
+0)
 system
 ;;
 *)
