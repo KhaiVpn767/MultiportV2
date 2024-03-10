26775d471d1236647788c5ce502ac1be4c71fde2
KhaiVpn767 <112326124+KhaiVpn767@users.noreply.github.com>
2024-03-10 17:06:36 +0800

    Update setup2.sh

--- setup2.sh
+++ setup2.sh
@@ -195,6 +195,14 @@ sleep 2
 wget https://raw.githubusercontent.com/KhaiVpn767/MultiportV5/main/set-br.sh && chmod +x set-br.sh && ./set-br.sh
 echo -e "\e[0;32mDONE INSTALLING SET-BR...\e[0m"
 clear
+#install UDP-CUSTOM
+echo -e "\e[33m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
+echo -e "$Magenta     INSTALLING UDP-CUSTOM              $NC"
+echo -e "\e[33m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
+sleep 2
+wget https://raw.githubusercontent.com/KhaiVpn767/multiport/main/Tunnel/udp.sh && chmod +x udp.sh && ./udp.sh
+echo -e "\e[0;32mDONE INSTALLING UDP-CUSTOM...\e[0m"
+clear
 # set time GMT +8
 ln -fs /usr/share/zoneinfo/Asia/Kuala_Lumpur /etc/localtime
 # install clouflare JQ
