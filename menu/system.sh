105c678d0517f83eefda00b3cd80bedf96c05c2c
KhaiVpn767 <112326124+KhaiVpn767@users.noreply.github.com>
2024-03-12 06:06:38 +0800

    Update system.sh

--- menu/system.sh
+++ menu/system.sh
@@ -29,22 +29,21 @@ echo -e "   \e[$line════════════════════
 echo -e "   \e[$number (•1)\e[m \e[$below Add New Subdomain\e[m"
 echo -e "   \e[$number (•2)\e[m \e[$below Renew Cert Xray Core\e[m"
 echo -e "   \e[$number (•3)\e[m \e[$below DNS Changer\e[m"
-echo -e "   \e[$number (•4)\e[m \e[$below Netflix Checker\e[m"
-echo -e "   \e[$number (•5)\e[m \e[$below Panel Domain\e[m"
-echo -e "   \e[$number (•6)\e[m \e[$below Backup Vps\e[m"
-echo -e "   \e[$number (•7)\e[m \e[$below Restore Vps\e[m"
-echo -e "   \e[$number (•8)\e[m \e[$below Install Webmin\e[m"
-echo -e "   \e[$number (•9)\e[m \e[$below Setup Speed VPS\e[m"
-echo -e "   \e[$number (10)\e[m \e[$below Restart VPN\e[m"
-echo -e "   \e[$number (11)\e[m \e[$below Speedtest VPS\e[m"
-echo -e "   \e[$number (12)\e[m \e[$below Xray Version\e[m"
-echo -e "   \e[$number (13)\e[m \e[$below Install BBR\e[m"
-echo -e "   \e[$number (14)\e[m \e[$below Install Ads Block\e[m"
-echo -e "   \e[$number (15)\e[m \e[$below Ads Block Panel\e[m"
-echo -e "   \e[$number (16)\e[m \e[$below ON/OF Auto Reboot\e[m"
-echo -e "   \e[$number (17)\e[m \e[$below Change Password VPS\e[m"
-echo -e "   \e[$number (18)\e[m \e[$below Check CPU & RAM\e[m"
-echo -e "   \e[$number (19)\e[m \e[$below Change Banner SSH\e[m"
+echo -e "   \e[$number (•4)\e[m \e[$below Panel Domain\e[m"
+echo -e "   \e[$number (•5)\e[m \e[$below Backup Vps\e[m"
+echo -e "   \e[$number (•6)\e[m \e[$below Restore Vps\e[m"
+echo -e "   \e[$number (•7)\e[m \e[$below Install Webmin\e[m"
+echo -e "   \e[$number (•8)\e[m \e[$below Setup Speed VPS\e[m"
+echo -e "   \e[$number (•9)\e[m \e[$below Restart VPN\e[m"
+echo -e "   \e[$number (10)\e[m \e[$below Speedtest VPS\e[m"
+echo -e "   \e[$number (11)\e[m \e[$below Xray Version\e[m"
+echo -e "   \e[$number (12)\e[m \e[$below Install BBR\e[m"
+echo -e "   \e[$number (13)\e[m \e[$below Install Ads Block\e[m"
+echo -e "   \e[$number (14)\e[m \e[$below Ads Block Panel\e[m"
+echo -e "   \e[$number (15)\e[m \e[$below ON/OF Auto Reboot\e[m"
+echo -e "   \e[$number (16)\e[m \e[$below Change Password VPS\e[m"
+echo -e "   \e[$number (17)\e[m \e[$below Check CPU & RAM\e[m"
+echo -e "   \e[$number (18)\e[m \e[$below Change Banner SSH\e[m"
 echo -e ""
 echo -e "   \e[$number (77)\e[m \e[$below ADD NS-DOMAIN/SlowDNS\e[m"
 echo -e "   \e[$number (88)\e[m \e[$below Install UDP Custom\e[m"
@@ -67,51 +66,48 @@ certv2ray
 dns
 ;;
 4)
-netf
-;;
-5)
 panel-domain
 ;;
-6)
+5)
 backup
 ;;
-7)
+6)
 restore
 ;;
-8)
+7)
 wbmn
 ;;
-9)
+8)
 limit-speed
 ;;
-10)
+9)
 restart
 ;;
-11)
+10)
 speedtest
 ;;
-12)
+11)
 xray version
 ;;
-13)
+12)
 bbr
 ;;
-14)
+13)
 ins-helium
 ;;
-15)
+14)
 helium
 ;;
-16)
+15)
 autoreboot
 ;;
-17)
+16)
 passwd
 ;;
-18)
+17)
 htop
 ;;
-19)
+18)
 message-ssh
 ;;
 77)
